a04ddcf496129afcd7032b36be9fb0ec9dc64133
zxk <2289249920@qq.com>
2019-07-05 17:58:39 +0800

    a

--- ConsoleClassName/Methord.cs
+++ ConsoleClassName/Methord.cs
@@ -5,34 +5,5 @@ using System.Text;
 
 namespace ConsoleClassName
 {
-    class Computer
-    {
-       /* public class Animal
-        {
-            public  void Bark()
-            {
-                Console.WriteLine("呜呜");
-            }
-        }
-        public class Cat : Animal
-        {
-            public new void Bark()
-            {
-                Console.WriteLine("喵喵");
-            }
-        }
-        class Porgram
-        {
-            static void Main(string[] args)
-            {
-                Cat cat = new Cat();
-                cat.Bark();
-                Animal animal = new Animal();
-                animal.Bark();
-
-                Animal animaljack = new Cat();
-                animaljack.Bark();
-            }
-        }*/
+   
     }
-}
--- ConsoleClassName/Program.cs
+++ ConsoleClassName/Program.cs
@@ -6,23 +6,5 @@ namespace ConsoleClassName
 {
     class Program
     {
-       /* static void Main(string[] args)
-        {
-            Type target = Type.GetType("ConditionComplier.Student");
-            object oTarget = target.GetConstructor(new Type[0]).Invoke(null);
-
-
-
-            Role[] roles = { Role.Sthdent, Role.Teacher };
-
-
-
-
-            //Type tStudent = typeof(Student);
-            //Console.WriteLine(tStudent.GetField("_age",
-            //    BindingFlags.NonPublic | BindingFlags.Instance)
-            //    .GetValue(new Student()));
-
-        }*/
     }
 }
