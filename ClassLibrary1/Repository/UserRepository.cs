2111f22c970f56f55ad96a17de55cd30e8f887ff
zxk <2289249920@qq.com>
2019-07-16 15:37:49 +0800

    更新

--- ClassLibrary1/Repository/UserRepository.cs
+++ ClassLibrary1/Repository/UserRepository.cs
@@ -1,26 +1,31 @@
 ﻿using Microsoft.EntityFrameworkCore;
-using System;
-using System.Collections.Generic;
 using System.Linq;
+using System;
 using System.Text;
-
 namespace BLL.Repository
 {
     public class UserRepository : DbContext
     {
 
-        private DbSet<User> _users;
+        public DbSet<User> _users { get; set;}
        public void Save(User user)
         {
+            
             _users.Add(user);
-            //储存
-            //Save(user);
             SaveChanges();
+            
         }
 
         public User GetByName(string name)
         {
+            
             return _users.Where(u => u.Name == name).SingleOrDefault();
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            string connectionString = @"Data Source=(localdb)\ProjectsV13;Initial Catalog=Great;Integrated Security=True;";
+            optionsBuilder.UseSqlServer(connectionString);
         }
     }
 }
