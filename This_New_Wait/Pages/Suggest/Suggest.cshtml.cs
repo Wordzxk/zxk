73f5b5bf136844bed03ce4b00c017795886c36cb
zxk <2289249920@qq.com>
2019-08-21 21:11:43 +0800

    添加多个页面---Layout页面的跳转

---
+++ This_New_Wait/Pages/Suggest/Suggest.cshtml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace This_New_Wait.Pages.Suggest
+{
+    public class SuggestModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+
+        public void OnPost()
+        {
+
+        }
+    }
+}
