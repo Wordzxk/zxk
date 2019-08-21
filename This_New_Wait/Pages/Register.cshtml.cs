b0f8b008452ccbb522a288509a387589a92a4b41
zxk <2289249920@qq.com>
2019-08-21 16:08:46 +0800

    页面未完成---进行部分更改....

---
+++ This_New_Wait/Pages/Register.cshtml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace This_New_Wait.Pages
+{
+    public class RegisterModel : PageModel
+    {
+
+        public void OnGet()
+        {
+           
+        }
+        public void OnPost()
+        {
+
+        }
+   
+    }
+}
